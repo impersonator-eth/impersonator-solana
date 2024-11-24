eeb02de922a10bede65674762ddb88d6109e4bd6
apoorvlathey <latheyapoorv@gmail.com>
2024-11-25 03:11:38 +0400

    reject request on modal close

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -6,15 +6,37 @@ import SessionProposalModal from "@/src/views/SessionProposalModal";
 import SessionSignModal from "@/src/views/SessionSignModal";
 import SessionSendTransactionModal from "@/src/views/SessionSendTransactionModal";
 import SessionUnsuportedMethodModal from "@/src/views/SessionUnsuportedMethodModal";
+import { rejectEIP155Request } from "@/src/utils/EIP155RequestHandlerUtil";
+import { web3wallet } from "@/src/utils/WalletConnectUtil";
 
 export default function Modal() {
   const { open, view } = useSnapshot(ModalStore.state);
 
-  const onClose = useCallback(() => {
-    if (open) {
+  // Get request and wallet data from store
+  const requestEvent = ModalStore.state.data?.requestEvent;
+  const requestSession = ModalStore.state.data?.requestSession;
+
+  const topic = requestEvent?.topic;
+  const params = requestEvent?.params;
+
+  // Handle reject action
+  const onReject = useCallback(async () => {
+    if (requestEvent && topic && open) {
+      const response = rejectEIP155Request(requestEvent);
+      try {
+        await web3wallet.respondSessionRequest({
+          topic,
+          response,
+        });
+      } catch (e) {
+        console.log((e as Error).message, "error");
+        return;
+      }
+      ModalStore.close();
+    } else if (open) {
       ModalStore.close();
     }
-  }, [open]);
+  }, [requestEvent, topic, open]);
 
   const componentView = useMemo(() => {
     switch (view) {
@@ -32,7 +54,7 @@ export default function Modal() {
   }, [view]);
 
   return (
-    <CModal isOpen={open} onClose={onClose} isCentered>
+    <CModal isOpen={open} onClose={onReject} isCentered>
       <ModalOverlay bg="none" backdropFilter="auto" backdropBlur="3px" />
       {componentView}
     </CModal>
