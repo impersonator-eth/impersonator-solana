981a66fee9590c7d752481569de7125020972360
apoorvlathey <latheyapoorv@gmail.com>
2024-11-25 17:44:57 +0400

    add meta tags + img

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import Analytics from "@/components/Analytics";
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 const _metadata = {
-  title: "Impersonator",
-  description:
-    "Impersonate any Ethereum Account and Login into DApps via WalletConnect, iframe or Browser extension!",
+  title: "Solana - Impersonator",
+  description: "Connect to Solana dapps as any Address!",
+  images: "https://solana.impersonator.xyz/metaIMG.png",
 };
 
 export const metadata: Metadata = {
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   description: _metadata.description,
   twitter: {
     card: "summary_large_image",
-    creator: "@apoorvlathey",
+    creator: "@apoorveth",
     title: _metadata.title,
     description: _metadata.description,
   },
